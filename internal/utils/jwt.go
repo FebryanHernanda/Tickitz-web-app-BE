f20cd46fa99017751b8a5b4f461817a192418270
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-08 08:53:29 +0700

    feat(utils): add generate jwt

---
+++ internal/utils/jwt.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/models"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTManager struct {
+	secret []byte
+}
+
+func NewJWTManager(secret string) *JWTManager {
+	return &JWTManager{
+		secret: []byte(secret),
+	}
+}
+
+type Claims struct {
+	UserID int    `json:"user_id"`
+	Email  string `json:"email"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+// Generate Token
+func (j *JWTManager) GenerateToken(user *models.User) (string, error) {
+	claims := &Claims{
+		UserID: user.ID,
+		Email:  user.Email,
+		Role:   user.Role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(30 * time.Minute)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Issuer:    "tickitz",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(j.secret)
+}
+
+// VerifyToken (parsing and validate)
+func (j *JWTManager) ValidateToken(tokenStr string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return j.secret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if !token.Valid {
+		return nil, jwt.ErrTokenInvalidClaims
+	}
+	return claims, nil
+}
