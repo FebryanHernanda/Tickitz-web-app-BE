88dce21266ec4d46da2206209c89d3f7ae3c2788
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-08 08:31:32 +0700

    feat(handler/movies): add movies handler (getupcomingmovies, getpopularmovies, getmoviesbyfilter, getschedulesmovies and getdetailmovies)

---
+++ internal/handlers/movies.handler.go
@@ -0,0 +1,212 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/repositories"
+	"github.com/gin-gonic/gin"
+)
+
+type MoviesHandler struct {
+	repo *repositories.MoviesRepository
+}
+
+func NewMoviesHandler(repo *repositories.MoviesRepository) *MoviesHandler {
+	return &MoviesHandler{
+		repo: repo,
+	}
+}
+
+// GetUpcomingMovies godoc
+// @Summary Get upcoming movies
+// @Description Retrieve a list of upcoming movies
+// @Tags Movies
+// @Produce json
+// @Success 200 {object} models.SuccessResponse
+// @Failure 404 {object} models.ErrorResponse
+// @Failure 500 {object} models.ErrorResponse
+// @Router /movies/upcoming [get]
+func (h *MoviesHandler) GetUpcomingMovies(ctx *gin.Context) {
+
+	movies, err := h.repo.GetUpcomingMovies(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	if len(movies) == 0 {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"success": false,
+			"data":    []any{},
+			"message": "No upcoming movies found",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data":    movies,
+	})
+}
+
+// GetPopularMovies godoc
+// @Summary Get popular movies
+// @Description Retrieve a list of popular movies
+// @Tags Movies
+// @Produce json
+// @Success 200 {object} models.SuccessResponse
+// @Failure 404 {object} models.ErrorResponse
+// @Failure 500 {object} models.ErrorResponse
+// @Router /movies/popular [get]
+func (h *MoviesHandler) GetPopularMovies(ctx *gin.Context) {
+
+	movies, err := h.repo.GetPopularMovies(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   err.Error(),
+		})
+	}
+
+	if len(movies) == 0 {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"success": false,
+			"data":    []any{},
+			"message": "No popular movies found",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data":    movies,
+	})
+}
+
+// GetMoviesByFilter godoc
+// @Summary Get movies by filter
+// @Description Retrieve movies filtered by search keyword and genre with pagination
+// @Tags Movies
+// @Produce json
+// @Param search query string false "Search keyword"
+// @Param genre query string false "Genre filter"
+// @Param page query int false "Page number (default 1)"
+// @Success 200 {object} models.SuccessResponse
+// @Failure 404 {object} models.ErrorResponse
+// @Failure 500 {object} models.ErrorResponse
+// @Router /movies [get]
+func (h *MoviesHandler) GetMoviesByFilter(ctx *gin.Context) {
+	search := ctx.Query("search")
+	genre := ctx.Query("genre")
+
+	page, err := strconv.Atoi(ctx.Query("page"))
+	if err != nil || page < 1 {
+		page = 1
+	}
+
+	limit := 5
+
+	offset := (page - 1) * limit
+
+	movies, err := h.repo.GetMoviesByFilter(ctx, search, genre, page, limit, offset)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	if len(movies) == 0 {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"success": false,
+			"data":    []any{},
+			"message": "No movies found",
+			"page":    page,
+			"limit":   limit,
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"page":    page,
+		"limit":   limit,
+		"count":   len(movies),
+		"data":    movies,
+	})
+}
+
+// GetDetailMovies godoc
+// @Summary Get movie detail
+// @Description Retrieve detailed information of a movie by ID
+// @Tags Movies
+// @Produce json
+// @Param id path int true "Movie ID"
+// @Success 200 {object} models.SuccessResponse
+// @Failure 400 {object} models.ErrorResponse
+// @Failure 404 {object} models.ErrorResponse
+// @Router /movies/{id}/details [get]
+func (h *MoviesHandler) GetDetailMovies(ctx *gin.Context) {
+
+	idParam := ctx.Param("id")
+	movieID, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil || movieID < 1 {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"error":   "Invalid movie ID",
+		})
+		return
+	}
+
+	movies, err := h.repo.GetDetailMovies(ctx, movieID)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"success": false,
+			"error":   "Movie not found",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data":    movies,
+	})
+}
+
+// GetSchedulesMovies godoc
+// @Summary Get movie schedules
+// @Description Retrieve the schedule of movies; returns 404 if no schedules found
+// @Tags Movies
+// @Produce json
+// @Success 200 {object} models.SuccessResponse
+// @Failure 404 {object} models.ErrorResponse
+// @Failure 500 {object} models.ErrorResponse
+// @Router /movies/schedules [get]
+func (h *MoviesHandler) GetSchedulesMovies(ctx *gin.Context) {
+	schedules, err := h.repo.GetSchedulesMovies(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	if len(schedules) == 0 {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"success": false,
+			"message": "No schedules movies found",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data":    schedules,
+	})
+}
