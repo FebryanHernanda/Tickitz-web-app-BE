c3b5ebc2f960737d381d110b4f653108ebcce80a
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-09 20:16:25 +0700

    feat(router/orders): add protected root for jwt token

--- internal/routers/orders.router.go
+++ internal/routers/orders.router.go
@@ -2,11 +2,15 @@ package routers
 
 import (
 	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/handlers"
+	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/middlewares"
+	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/utils"
 	"github.com/gin-gonic/gin"
 )
 
-func OrdersRouter(r *gin.Engine, ordersHandler *handlers.OrdersHandler) {
+func OrdersRouter(r *gin.Engine, ordersHandler *handlers.OrdersHandler, jwtManager *utils.JWTManager) {
 	ordersRoutes := r.Group("/orders")
+	ordersRoutes.Use(middlewares.VerifyToken(jwtManager))
+	ordersRoutes.Use(middlewares.AuthMiddleware("user"))
 	ordersRoutes.POST("/", ordersHandler.CreateOrder)
-	ordersRoutes.GET("/history/:userID", ordersHandler.GetOrdersHistory)
+	ordersRoutes.GET("/history", ordersHandler.GetOrdersHistory)
 }
