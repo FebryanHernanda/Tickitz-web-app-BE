bcd28e4084fa2a745eb5da51e1f8cbdddd23e0ba
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-09 22:11:34 +0700

    feat(models/profile): add UserUpdateRequest struct for combine the user and profile update

--- internal/routers/profile.router.go
+++ internal/routers/profile.router.go
@@ -11,5 +11,7 @@ func ProfileRouter(r *gin.Engine, profileHandler *handlers.ProfileHandler, jwtMa
 	profileRoutes := r.Group("/profile")
 	profileRoutes.Use(middlewares.VerifyToken(jwtManager))
 	profileRoutes.Use(middlewares.AuthMiddleware("user"))
+
 	profileRoutes.GET("", profileHandler.GetProfile)
+	profileRoutes.PATCH("/edit", profileHandler.UpdateProfile)
 }
