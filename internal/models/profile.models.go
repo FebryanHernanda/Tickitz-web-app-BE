80d9608b53a3c585df154c3fdfcc1cc25eab326b
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-09 20:05:34 +0700

    feat(models/profile): add profile models

---
+++ internal/models/profile.models.go
@@ -0,0 +1,21 @@
+package models
+
+type Profile struct {
+	ID             int     `json:"id"`
+	Email          string  `json:"email"`
+	Role           string  `json:"role"`
+	VirtualAccount string  `json:"virtual_account"`
+	FirstName      *string `json:"first_name"`
+	LastName       *string `json:"last_name"`
+	PhoneNumber    *string `json:"phone_number"`
+	Points         *int    `json:"points"`
+	ImagePath      *string `json:"image_path"`
+}
+
+type ProfileUpdate struct {
+	FirstName   *string `form:"first_name" json:"first_name"`
+	LastName    *string `form:"last_name" json:"last_name"`
+	PhoneNumber *string `form:"phone_number" json:"phone_number"`
+	ImagePath   *string `json:"image_path"`
+	Points      *int    `json:"points"`
+}
