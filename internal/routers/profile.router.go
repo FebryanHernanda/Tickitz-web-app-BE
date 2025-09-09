87acf5460008201032a5119a17a2011315c3ed78
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-09 20:17:22 +0700

    feat(router/profile): add protected root for jwt token

--- internal/routers/profile.router.go
+++ internal/routers/profile.router.go
@@ -2,9 +2,14 @@ package routers
 
 import (
 	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/handlers"
+	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/middlewares"
+	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/utils"
 	"github.com/gin-gonic/gin"
 )
 
-func ProfileRouter(r *gin.Engine, profileHandler *handlers.ProfileHandler) {
-	r.GET("/profile/:userID", profileHandler.GetProfile)
+func ProfileRouter(r *gin.Engine, profileHandler *handlers.ProfileHandler, jwtManager *utils.JWTManager) {
+	profileRoutes := r.Group("/profile")
+	profileRoutes.Use(middlewares.VerifyToken(jwtManager))
+	profileRoutes.Use(middlewares.AuthMiddleware("user"))
+	profileRoutes.GET("", profileHandler.GetProfile)
 }
