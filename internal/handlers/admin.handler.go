e9881c69029a4202cef14127cb044ae5a9a91976
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-10 22:00:29 +0700

    -feat(handler/admin): add updateMovies and refactor swagger on addMovies handler(add fileupload, genre, cast, schedule parsing data form multipart/form-data)

--- internal/handlers/admin.handler.go
+++ internal/handlers/admin.handler.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/models"
 	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/repositories"
+	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -56,15 +59,27 @@ func (a *AdminHandler) GetAllMovies(ctx *gin.Context) {
 	})
 }
 
-// GetAllMovies godoc
-// @Summary      Add Movies
+// AddMovie godoc
+// @Summary      Add New Movie
 // @Description  Add Movies with all the relations (genres, cast, and schedules)
 // @Tags         Admin
 // @Security     BearerAuth
+// @Accept       multipart/form-data
 // @Produce      json
-// @Param        user  body      models.AddMovies  true  "Add Movies data"
-// @Success      200  {object}  models.SuccessResponse
-// @Failure      400  {object}  models.ErrorResponse
+// @Param        title         formData  string  false  "Movie Title"
+// @Param        synopsis      formData  string  false  "Movie Synopsis"
+// @Param        release_date  formData  string  false  "Release Date (YYYY-MM-DD)"
+// @Param        rating        formData  number  false  "Movie Rating"
+// @Param        age_rating    formData  string  false  "Age Rating"
+// @Param        duration      formData  int     false  "Duration (minutes)"
+// @Param        director_id   formData  int     false  "Director ID"
+// @Param        genres        formData  string   false  "Genres [IDs, comma separated]"
+// @Param        casts         formData  string   false  "Casts [IDs, comma separated]"
+// @Param        schedules     formData  string  false  "Schedules (JSON array: [{}])"
+// @Param        poster        formData  file    false  "Poster file"
+// @Param        backdrop      formData  file    false  "Backdrop file"
+// @Success      200           {object}  models.SuccessResponse
+// @Failure      400           {object}  models.ErrorResponse
 // @Failure      401  {object}  models.ErrorResponse
 // @Failure      500  {object}  models.ErrorResponse
 // @Router       /admin/movies/add [post]
@@ -75,6 +90,66 @@ func (h *AdminHandler) AddMovies(ctx *gin.Context) {
 			"success": false,
 			"error":   err.Error(),
 		})
+	}
+
+	posterPath, err := utils.UploadFile(ctx, "poster", "public/movies/posters", "poster")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   "Failed to upload poster"})
+		return
+	}
+	if posterPath != "" {
+		movie.PosterPath = posterPath
+	}
+
+	backdropPath, err := utils.UploadFile(ctx, "backdrop", "public/movies/backdrops", "backdrop")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   "Failed to upload backdrop",
+		})
+		return
+	}
+	if backdropPath != "" {
+		movie.BackdropPath = backdropPath
+	}
+
+	if genresStr := ctx.PostForm("genres"); genresStr != "" {
+		var genres []int
+		if err := json.Unmarshal([]byte(genresStr), &genres); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error":   "invalid genres format, must be JSON array like [1,2,3]",
+			})
+			return
+		}
+		movie.Genres = genres
+	}
+
+	if castsStr := ctx.PostForm("casts"); castsStr != "" {
+		var casts []int
+		if err := json.Unmarshal([]byte(castsStr), &casts); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error":   "invalid casts format, must be JSON array like [1,2,3]",
+			})
+			return
+		}
+		movie.Casts = casts
+	}
+
+	schedulesStr := ctx.PostForm("schedules")
+	if schedulesStr != "" {
+		var schedules []models.Schedule
+		if err := json.Unmarshal([]byte(schedulesStr), &schedules); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error":   "invalid schedules format",
+			})
+			return
+		}
+		movie.Schedules = schedules
 	}
 
 	movieData, err := h.repo.AddMovies(ctx, &movie)
@@ -94,7 +169,7 @@ func (h *AdminHandler) AddMovies(ctx *gin.Context) {
 }
 
 // GetAllMovies godoc
-// @Summary      Get movies schedules for check the id to add cinemas schedule
+// @Summary      Get movies schedules
 // @Description  Get movies schedules for check the id to add cinemas schedule
 // @Tags         Admin
 // @Security     BearerAuth
@@ -116,6 +191,119 @@ func (h *AdminHandler) GetMovieSchedule(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"success": true,
 		"data":    schedules,
+	})
+}
+
+// UpdateMovie godoc
+// @Summary      Update movie with file upload
+// @Description  Update movie data by ID, allow uploading poster and backdrop
+// @Tags         Admin
+// @Security     BearerAuth
+// @Accept       multipart/form-data
+// @Produce      json
+// @Param        id   			path      int  true  "Movie ID"
+// @Param        title         formData  string  false  "Movie Title"
+// @Param        synopsis      formData  string  false  "Movie Synopsis"
+// @Param        release_date  formData  string  false  "Release Date (YYYY-MM-DD)"
+// @Param        rating        formData  number  false  "Movie Rating"
+// @Param        age_rating    formData  string  false  "Age Rating"
+// @Param        duration      formData  int     false  "Duration (minutes)"
+// @Param        director_id   formData  int     false  "Director ID"
+// @Param        genres        formData  string   false  "Genres [IDs, comma separated]"
+// @Param        casts         formData  string   false  "Casts [IDs, comma separated]"
+// @Param        schedules     formData  string  false  "Schedules (JSON array: [{}])"
+// @Param        poster        formData  file    false  "Poster file"
+// @Param        backdrop      formData  file    false  "Backdrop file"
+// @Success      200           {object}  models.SuccessResponse
+// @Failure      400           {object}  models.ErrorResponse
+// @Failure      401  		   {object}  models.ErrorResponse
+// @Failure      500           {object}  models.ErrorResponse
+// @Router       /admin/movies/edit/{id} [patch]
+func (h *AdminHandler) UpdateMovies(ctx *gin.Context) {
+	MovieID, _ := strconv.Atoi(ctx.Param("id"))
+
+	var update models.EditMovies
+	if err := ctx.ShouldBind(&update); err != nil {
+
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	posterPath, err := utils.UploadFile(ctx, "poster", "public/movies/posters", "poster")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   "Failed to upload poster"})
+		return
+	}
+	if posterPath != "" {
+		update.PosterPath = &posterPath
+	}
+
+	backdropPath, err := utils.UploadFile(ctx, "backdrop", "public/movies/backdrops", "backdrop")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   "Failed to upload backdrop",
+		})
+		return
+	}
+	if backdropPath != "" {
+		update.BackdropPath = &backdropPath
+	}
+
+	if genresStr := ctx.PostForm("genres"); genresStr != "" {
+		var genres []int
+		if err := json.Unmarshal([]byte(genresStr), &genres); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error":   "invalid genres format, must be JSON array like [1,2,3]",
+			})
+			return
+		}
+		update.Genres = &genres
+	}
+
+	if castsStr := ctx.PostForm("casts"); castsStr != "" {
+		var casts []int
+		if err := json.Unmarshal([]byte(castsStr), &casts); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error":   "invalid casts format, must be JSON array like [1,2,3]",
+			})
+			return
+		}
+		update.Casts = &casts
+	}
+
+	schedulesStr := ctx.PostForm("schedules")
+	if schedulesStr != "" {
+		var schedules []models.Schedule
+		if err := json.Unmarshal([]byte(schedulesStr), &schedules); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+				"error":   "invalid schedules format",
+			})
+			return
+		}
+		update.Schedules = &schedules
+	}
+
+	if err := h.repo.UpdateMovies(ctx, MovieID, update); err != nil {
+		log.Printf("%s", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "movie updated successfully",
 	})
 }
 
