e6b8f2a328f151eb3feb6e4d5d3e2eea3580de4b
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-12 21:49:38 +0700

    feat(router/cinema): add cinema router

---
+++ internal/routers/cinema.router.go
@@ -0,0 +1,13 @@
+package routers
+
+import (
+	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func CinemaRouter(r *gin.Engine, cinemaHandler *handlers.CinemaHandler) {
+	cinemaRoutes := r.Group("/cinemas")
+
+	cinemaRoutes.GET("/available-seats/:cinemas_schedule_id", cinemaHandler.GetAvailableSeats)
+	cinemaRoutes.GET("/", cinemaHandler.GetScheduleFilter)
+}
