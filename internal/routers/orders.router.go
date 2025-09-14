ee7611a59a0ae276b06a5527e390c9d2b4a2c23a
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-14 21:33:24 +0700

    feat(router/orders): add redis on router

--- internal/routers/orders.router.go
+++ internal/routers/orders.router.go
@@ -5,11 +5,12 @@ import (
 	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/middlewares"
 	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
-func OrdersRouter(r *gin.Engine, ordersHandler *handlers.OrdersHandler, jwtManager *utils.JWTManager) {
+func OrdersRouter(r *gin.Engine, ordersHandler *handlers.OrdersHandler, jwtManager *utils.JWTManager, rdb *redis.Client) {
 	ordersRoutes := r.Group("/orders")
-	ordersRoutes.Use(middlewares.VerifyToken(jwtManager))
+	ordersRoutes.Use(middlewares.VerifyToken(jwtManager, rdb))
 	ordersRoutes.Use(middlewares.AuthMiddleware("user"))
 	ordersRoutes.POST("/", ordersHandler.CreateOrder)
 	ordersRoutes.GET("/history", ordersHandler.GetOrdersHistory)
