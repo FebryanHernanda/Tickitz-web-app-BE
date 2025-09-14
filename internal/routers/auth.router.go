ab8d0803c42b21670b41e4538e67a3617a04fbf0
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-14 21:33:08 +0700

    feat(router/auth): add redis on router

--- internal/routers/auth.router.go
+++ internal/routers/auth.router.go
@@ -2,11 +2,15 @@ package routers
 
 import (
 	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/handlers"
+	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/middlewares"
+	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
-func AuthRouter(r *gin.Engine, authHandler *handlers.AuthHandler) {
+func AuthRouter(r *gin.Engine, jwtManager *utils.JWTManager, rdb *redis.Client, authHandler *handlers.AuthHandler) {
 	authRoutes := r.Group("/auth")
 	authRoutes.POST("/register", authHandler.Register)
 	authRoutes.POST("/login", authHandler.Login)
+	authRoutes.POST("/logout", middlewares.VerifyToken(jwtManager, rdb), middlewares.AuthMiddleware("user", "admin"), authHandler.Logout)
 }
