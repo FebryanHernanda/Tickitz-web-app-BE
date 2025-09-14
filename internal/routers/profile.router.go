0379418dd1c0e307626702db573a71767efa8a3b
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-14 21:33:51 +0700

    feat(router/profile): add redis on router

--- internal/routers/profile.router.go
+++ internal/routers/profile.router.go
@@ -5,11 +5,12 @@ import (
 	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/middlewares"
 	"github.com/FebryanHernanda/Tickitz-web-app-BE/internal/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
-func ProfileRouter(r *gin.Engine, profileHandler *handlers.ProfileHandler, jwtManager *utils.JWTManager) {
+func ProfileRouter(r *gin.Engine, profileHandler *handlers.ProfileHandler, jwtManager *utils.JWTManager, rdb *redis.Client) {
 	profileRoutes := r.Group("/profile")
-	profileRoutes.Use(middlewares.VerifyToken(jwtManager))
+	profileRoutes.Use(middlewares.VerifyToken(jwtManager, rdb))
 	profileRoutes.Use(middlewares.AuthMiddleware("user"))
 
 	profileRoutes.GET("", profileHandler.GetProfile)
