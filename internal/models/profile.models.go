36905dc4f2c53d167683b139751d66d1bf7b7285
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-18 10:36:44 +0700

    feat(models/profile): add tag for multipart-form

--- internal/models/profile.models.go
+++ internal/models/profile.models.go
@@ -17,14 +17,15 @@ type UserUpdate struct {
 	Email       *string `form:"email" json:"email"`
 	OldPassword *string `form:"old_password" json:"old_password"`
 	NewPassword *string `form:"new_password" json:"new_password"`
+	Password    *string `json:"-"`
 }
 
 type ProfileUpdate struct {
 	FirstName   *string `form:"first_name" json:"first_name"`
 	LastName    *string `form:"last_name" json:"last_name"`
 	PhoneNumber *string `form:"phone_number" json:"phone_number"`
-	ImagePath   *string `json:"image_path"`
-	Points      *int    `json:"points"`
+	ImagePath   *string `form:"image_path" json:"image_path"`
+	Points      *int    `form:"points" json:"points"`
 }
 
 type UserUpdateRequest struct {
