afbb536d1b90d8b6563f87ede64e5b13ad0e78ae
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-21 11:27:26 +0700

    feat(models/admin): add struct models for updateMovies data

--- internal/models/admin.models.go
+++ internal/models/admin.models.go
@@ -22,6 +22,7 @@ type AdminMovies struct {
 }
 
 type AddMovies struct {
+	ID           int        `json:"id,omitempty"`
 	Title        string     `form:"title" json:"title" example:"Negeri 5 Menara"`
 	PosterPath   string     `form:"poster_path" json:"poster_path" example:"/path/poster.jpg"`
 	BackdropPath string     `form:"backdrop_path" json:"backdrop_path" example:"/path/backdrop.jpg"`
@@ -34,21 +35,23 @@ type AddMovies struct {
 	Genres       []int      `json:"genres" example:"1"`
 	Casts        []int      `json:"casts" example:"2"`
 	Schedules    []Schedule `json:"schedules"`
+	ScheduleIDs  []int      `json:"schedule_ids,omitempty"`
 }
 
 type EditMovies struct {
-	Title        *string     `form:"title" json:"title,omitempty" example:"Negeri 5 Menara"`
-	PosterPath   *string     `form:"poster_path" json:"poster_path,omitempty" example:"/path/poster.jpg"`
-	BackdropPath *string     `form:"backdrop_path" json:"backdrop_path,omitempty" example:"/path/backdrop.jpg"`
-	Synopsis     *string     `form:"synopsis" json:"synopsis,omitempty" example:"Negeri 5 Menara merupakan film yang..."`
-	ReleaseDate  *string     `form:"release_date" json:"release_date,omitempty" example:"2025-09-01"`
-	Rating       *float32    `form:"rating" json:"rating,omitempty" example:"7.5"`
-	AgeRating    *string     `form:"age_rating" json:"age_rating,omitempty" example:"R"`
-	Duration     *int        `form:"duration" json:"duration,omitempty" example:"120"`
-	DirectorID   *int        `form:"director_id" json:"director_id,omitempty" example:"1"`
-	Genres       *[]int      `json:"genres,omitempty" example:"1"`
-	Casts        *[]int      `json:"casts,omitempty" example:"2"`
-	Schedules    *[]Schedule `json:"schedules,omitempty"`
+	Title           *string                   `form:"title" json:"title,omitempty" example:"Negeri 5 Menara"`
+	PosterPath      *string                   `form:"poster_path" json:"poster_path,omitempty" example:"/path/poster.jpg"`
+	BackdropPath    *string                   `form:"backdrop_path" json:"backdrop_path,omitempty" example:"/path/backdrop.jpg"`
+	Synopsis        *string                   `form:"synopsis" json:"synopsis,omitempty" example:"Negeri 5 Menara merupakan film yang..."`
+	ReleaseDate     *string                   `form:"release_date" json:"release_date,omitempty" example:"2025-09-01"`
+	Rating          *float32                  `form:"rating" json:"rating,omitempty" example:"7.5"`
+	AgeRating       *string                   `form:"age_rating" json:"age_rating,omitempty" example:"R"`
+	Duration        *int                      `form:"duration" json:"duration,omitempty" example:"120"`
+	DirectorID      *int                      `form:"director_id" json:"director_id,omitempty" example:"1"`
+	Genres          *[]int                    `json:"genres,omitempty" example:"1"`
+	Casts           *[]int                    `json:"casts,omitempty" example:"2"`
+	Schedules       *[]Schedule               `json:"schedules,omitempty"`
+	CinemaSchedules *[]CinemaScheduleLocation `json:"cinemas_schedules"`
 }
 
 type Schedule struct {
@@ -57,9 +60,11 @@ type Schedule struct {
 }
 
 type CinemaScheduleLocation struct {
-	CinemaID   int `json:"cinema_id"`
-	ScheduleID int `json:"schedule_id"`
-	LocationID int `json:"location_id"`
+	CinemaID   int64  `json:"cinemas_id" example:"1"`
+	LocationID int64  `json:"locations_id" example:"2"`
+	ScheduleID int    `json:"schedules_id" example:"85"`
+	Date       string `json:"date"`
+	Time       string `json:"time"`
 }
 
 type GetSchedule struct {
@@ -68,4 +73,39 @@ type GetSchedule struct {
 	Time      string    `json:"time" example:"18:00"`
 	MovieID   int       `json:"movie_id" example:"1"`
 	MovieName string    `json:"title"`
+}
+
+/* For get Movies Detail */
+type ScheduleDetails struct {
+	Date  string   `json:"date"`
+	Times []string `json:"times"`
+}
+
+type MovieEditDetail struct {
+	ID           int64     `json:"id"`
+	Title        string    `json:"title"`
+	Synopsis     string    `json:"synopsis"`
+	ReleaseDate  time.Time `json:"release_date"`
+	Duration     int       `json:"duration"`
+	Rating       float64   `json:"rating"`
+	AgeRating    string    `json:"age_rating"`
+	PosterPath   string    `json:"poster_path"`
+	BackdropPath string    `json:"backdrop_path"`
+	DirectorID   int64     `json:"director_id"`
+	DirectorName string    `json:"director_name"`
+	GenreIDs     []int64   `json:"genre_ids"`
+	GenreNames   []string  `json:"genre_names"`
+	CastIDs      []int64   `json:"cast_ids"`
+	CastNames    []string  `json:"cast_names"`
+	Schedules    []struct {
+		Date  string   `json:"date"`
+		Times []string `json:"times"`
+	} `json:"schedules"`
+	CinemaSchedules []CinemaScheduleLocation `json:"cinema_schedules"`
+}
+
+type ScheduleDB struct {
+	ID   int    `json:"id"`
+	Date string `json:"date"`
+	Time string `json:"time"`
 }
