5e1a0acfcfb472cba8fd0631ab1aaa1ffa036291
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-21 11:38:46 +0700

    feat(router/movies): add get genres, cast, directors router

--- internal/routers/movies.router.go
+++ internal/routers/movies.router.go
@@ -7,6 +7,9 @@ import (
 
 func MoviesRouter(r *gin.Engine, moviesHandler *handlers.MoviesHandler) {
 	moviesRoutes := r.Group("/movies")
+	moviesRoutes.GET("/genres", moviesHandler.GetGenresMovies)
+	moviesRoutes.GET("/casts", moviesHandler.GetCastsMovies)
+	moviesRoutes.GET("/directors", moviesHandler.GetDirectorsMovies)
 	moviesRoutes.GET("/upcoming", moviesHandler.GetUpcomingMovies)
 	moviesRoutes.GET("/popular", moviesHandler.GetPopularMovies)
 	moviesRoutes.GET("/:id/details", moviesHandler.GetDetailMovies)
